8034f3bbfa0ca2ebf9f26018fabe87571f2bbccc
RohanM78 <100198235+RohanM78@users.noreply.github.com>
2025-07-25 18:52:13 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 if (typeof global !== "undefined" && typeof global.Buffer === "undefined") {
   global.Buffer = require("buffer").Buffer;
 }
-
 import { Tabs } from 'expo-router';
 import { Shield, Users, Settings } from 'lucide-react-native';
 
